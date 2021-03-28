d83f1d51214279941adbe82302606fa1b4e6ec82
MostlyArmless <mike.j.thiem@gmail.com>
2021-03-28 16:59:14 -0600

    Fixed linting error in test file

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 import { Debt } from '../Debt';
 import { IDebt, IIncome, IncomeStartCondition, IncomeEndCondition } from '../interfacesAndEnums';
 import { Income } from '../Income';
