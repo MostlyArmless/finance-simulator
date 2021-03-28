3c1f02ff03e1189143b59c82d77aeac2954c7ee9
MostlyArmless <mike.j.thiem@gmail.com>
2021-03-28 15:09:54 -0600

    Formatting change

--- scripts/mochatest.js
+++ scripts/mochatest.js
@@ -1,4 +1,4 @@
-const fse = require("fs-extra");
+const fse = require( "fs-extra" );
 // Because React's `npm start` changes the tsconfig to use esnext modules and the mocha test explorer vs code extension requires commonjs modules, you'll need to run this script
 // after having run `npm start` in order to be able to use the test explorer. You can't use/run both simultaneously.
 
@@ -6,10 +6,10 @@ console.log(
   `Setting tsconfig compilerOptions.module to "commonjs" for Mocha Test Explorer to work...`
 );
 const tsconfigFile = "./tsconfig.json";
-let tsconfigContents = fse.readJSONSync(tsconfigFile);
+let tsconfigContents = fse.readJSONSync( tsconfigFile );
 
 tsconfigContents.compilerOptions.module = "commonjs";
 
-fse.writeJsonSync(tsconfigFile, tsconfigContents, { spaces: 2 });
+fse.writeJsonSync( tsconfigFile, tsconfigContents, { spaces: 2 } );
 
-console.log("Done");
+console.log( "Done" );
