83b89115cd64b9b41d1111222afb830656e1afce
MostlyArmless <mike.j.thiem@gmail.com>
2021-03-30 15:57:20 -0600

    Added TODO comment

--- src/components/ScenarioSummaryComponent.tsx
+++ src/components/ScenarioSummaryComponent.tsx
@@ -1,6 +1,7 @@
 import { IScenarioSummary } from "../interfacesAndEnums";
 import { formatNumberAsDollars } from "../tools";
 
+// TODO - this component probably shouldn't have "Component" in the name, but I've already got a non-React class named ScenarioSummary, so what's the best practice in this situation?
 export function ScenarioSummaryComponent( props: { summary: IScenarioSummary } )
 {
     return (
